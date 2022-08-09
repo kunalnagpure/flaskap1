bb0aead0b4d66a26b6b413f91e0cc8d50aa12bbf
Kunal <kunalnagpure1989@gmail.com>
2022-08-09 18:35:32 +0530

    1st

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from flask import Flask
+from flask import Flask, request, json
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-    return "Hello, World!"
+    response = requests.post('https://httpbin.org/post', json={'id': 1, 'name': 'Kunal'})
+
+    print("Status code: ", response.status_code)
+    print("Printing Entire Post Request")
+    print(response.json())
 
 
-@app.route("/salvador")
-def salvador():
-    return "Hello, Salvador"
+with open('response', 'w') as json_file:
+    json.dump(response, json_file)
+
+
+
+@app.route("/Kunal")
+def Kunal():
+    return "Hello, Kunal"
 
 
 if __name__ == "__main__":
