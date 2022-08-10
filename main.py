933e17df254069341ba01c5ad1f912b65a1c3d34
Kunal <kunalnagpure1989@gmail.com>
2022-08-10 15:42:11 +0530

    2st

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+import os
+
+import requests
+import json
 from flask import Flask, request, json
 
 app = Flask(__name__)
 
+fpath = C:\Users\kunal.nagpure\PycharmProjects\flaskapp\personal.json
 @app.route("/")
+
 def home():
     response = requests.post('https://httpbin.org/post', json={'id': 1, 'name': 'Kunal'})
+    FileCheck(personal.json)
 
-    print("Status code: ", response.status_code)
-    print("Printing Entire Post Request")
-    print(response.json())
-
-
-with open('response', 'w') as json_file:
-    json.dump(response, json_file)
-
-
+def FileCheck(name):
+    if os.path.isfile(fpath) and os.access(fpath, os.W_OK):
+        # checks if file exists
+        print ("File exists and is writable")
+    else:
+        return "400"
+def Writetofile(data):
+    with open(os.path.join(fpath, 'personal.json'), 'w+') as file:
+        file.write(json.dumps())
 
 @app.route("/Kunal")
 def Kunal():
